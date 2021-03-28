03c464042756195016d41a2db42b357964e5fa86
Marco Thomas <mail@marco-thomas.net>
2021-03-28 20:03:59 +0200

    New font

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FiraCode Nerd Font Mono:size=13",
+	"Product Sans:size=13",
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
